31b5ef5781611e1710f37f59e9400221257cc96b
ebukaeze <wizzyblaq94@gmail.com>
2021-12-11 22:31:05 +0100

    import product component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Announcement from '../components/Announcement'
+import Categories from '../components/Categories';
+import Navbar from '../components/Navbar';
+import NewsLetter from '../components/NewsLetter';
+import Products from '../components/Products';
+import Slider from '../components/Slider';
+
+const Home = () => {
+    return (
+        <div>
+            <Announcement />
+           <Navbar />
+           <Slider />
+           <Categories />
+           <Products />
+           <NewsLetter />
+        </div>
+    )
+}
+
+export default Home
