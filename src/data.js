ae00c1b7b3a641866391f4bf8532e7582fad041e
ebukaeze <wizzyblaq94@gmail.com>
2021-12-11 22:27:26 +0100

    product data

---
+++ src/data.js
@@ -0,0 +1,115 @@
+import img1 from './assets/images/img2.jpg';
+import img2 from './assets/images/img1.jpg';
+import img3 from './assets/images/img3.jpg';
+import img4 from './assets/images/img4.jpg';
+
+
+
+export const slideItem = [
+    {
+        id: 1,
+        title: "Luxury fragrance",
+        desc: "Get the best perfume that puts a smile around you",
+        image: img1,
+        bg: "212,133,49",
+        btn: "Shop Now"
+
+    },
+    {
+        id: 2,
+        title: "Summer wears",
+        desc: "Get the best ouftits for this summer",
+        image: img2,
+        bg: "140, 215, 250",
+        btn: "Shop Now"
+
+    },
+    {
+        id: 3,
+        title: "Summer sale",
+        desc: "Get the best perfume that puts a smile around you",
+        image: img3,
+        bg: "173, 107, 6",
+        btn: "Shop Now"
+
+    },
+    {
+        id: 2,
+        title: "Summer wears",
+        desc: "Get the best ouftits for this summer",
+        image: img4,
+        bg: "255, 34, 34",
+        btn: "Shop Now"
+
+    },
+    
+
+]
+
+export const categories = [
+
+    {
+        id: 1,
+        image: img1,
+        title: "Shirt Style",
+        color: "216,180,109",
+
+    },
+    {
+        id: 2,
+        image: img2,
+        title: "Loungewear Love",
+        color: "140, 215, 250",
+
+    },
+    {
+        id: 3,
+        image: img3,
+        title: "Light Jackets",
+        color: "173, 107, 6",
+
+    },
+    {
+        id: 4,
+        image: img4,
+        title: "Beach style",
+        color: "212,133,49",
+
+    },
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        image: img1,
+    },
+    {
+        id: 2,
+        image: img2,
+    },
+    {
+        id: 3,
+        image: img3,
+    },
+    {
+        id: 4,
+        image: img4,
+    },
+    {
+        id: 5,
+        image: img1,
+    },
+    {
+        id: 6,
+        image: img3,
+    },
+    {
+        id: 7,
+        image: img4,
+    },
+     {
+        id: 8,
+        image: img1,
+    }
+
+]
