f9374277b29132b9057964b80e800babcbae4a43
Ebuka Victor Ezenwabasili <wizzyblaq94@gmail.com>
2021-12-16 02:47:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-
+import Home from './pages/Home';
+import Product from './pages/Product';
+import ProductList from './pages/ProductList';
+import Register from './pages/Register';
+ 
 function App() {
   return (
-    <div className="App">
-      <h1 style={{fontSize: '38px'}}> Welcome to Chocho</h1>
-      <p>testing the p tag</p>
+    <div>
+     {/* <Home /> */}
+     {/* <ProductList /> */}
+     {/* <Product /> */}
+     <Register />
     </div>
   );
 }
 
 export default App;
+
