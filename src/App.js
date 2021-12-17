d93a1923c3babfac31fb72e48035549f1f66c84b
ebukaeze <wizzyblaq94@gmail.com>
2021-12-17 01:12:48 +0100

    import components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
+import Cart from './pages/Cart';
 import Home from './pages/Home';
+import Login from './pages/Login';
 import Product from './pages/Product';
 import ProductList from './pages/ProductList';
 import Register from './pages/Register';
@@ -7,10 +9,12 @@ import Register from './pages/Register';
 function App() {
   return (
     <div>
-     {/* <Home /> */}
+     <Home />
      {/* <ProductList /> */}
      {/* <Product /> */}
-     <Register />
+     {/* <Register /> */}
+     {/* <Login /> */}
+     {/* <Cart /> */}
     </div>
   );
 }
