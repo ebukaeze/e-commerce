95fbfef8f20f5e0e77098e561290f85a124274e1
ebukaeze <wizzyblaq94@gmail.com>
2021-12-17 01:08:16 +0100

    created the login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
-import { Container } from '../components/styles/AuthStyle';
+import { Container, Wrapper, Form, Title, Input, Agreement, Button, Em } from '../components/styles/AuthStyle';
 
 const Login = () => {
+    const handleSubmit = (e) => {
+     e.preventDefault();
+    }
     return (
         <Container>
-            
+            <Navbar simple/>
+            <Wrapper>
+                
+                <Form>
+                    <Title>SIGN IN</Title>
+                 <Input placeholder='username'/>
+                 <Input type="password" placeholder='password'/>
+                 <Agreement>By signing up you agree to our terms and <Em>privacy</Em> policies</Agreement>
+                 <Button onClick={handleSubmit}>SIGN IN</Button>
+                </Form>
+            </Wrapper>
         </Container>
     )
 }
